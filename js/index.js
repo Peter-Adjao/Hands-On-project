3b019b47d93be1ab939d701ff02d1e4f943c4c54
Peter Adjao <pa376499@gmail.com>
2025-08-27 20:17:08 +0000

    chore: Access an object with dot(.) notation

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let numberOfCats = 3;
+
+console.log(numberOfCats);
+
+
+
+let myBook = {
+    title: "Good times",
+    author: "Peter Adjao",
+    numberOfPages: 250,
+    isAvailable: true
+};
+
+console.log(myBook.title);
+console.log(myBook.author);
