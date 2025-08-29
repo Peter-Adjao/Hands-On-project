a0ac06e37b69a938f2873e79e675d1a8065b6b34
Peter Adjao <pa376499@gmail.com>
2025-08-29 05:46:18 +0000

    feat: Add switch statement to control program flow

--- js/index.js
+++ js/index.js
@@ -79,3 +79,65 @@ let firstEpisode = new Episode("Beautful Beginnings", 45, 45);
 console.log(firstEpisode);
 
 
+//Switch statement
+let firstUser = {
+    name: "Korkro",
+    age: 33,
+    accountLevel: "normal"
+}
+
+let secondUser = {
+    name: "Amaina",
+    age: 22,
+    accountLevel: "premium"
+}
+
+let thirdUser = {
+    name: "Kojo",
+    age: 45,
+    accountLevel: "mega-premium"
+}
+
+switch(firstUser.accountLevel) {
+    case "normal":
+        console.log("You have a normal accoutn!");
+        break;
+    case "premium":
+        console.log("You have a premium account!");
+        break;
+
+    case "mega-premium":
+        console.log("You have mega-premium account!");
+        break;
+    default:
+        console.log("Unknown account type!");
+}
+
+
+
+
+
+/*
+const body = document.querySelector('body');
+
+for(let i = 0; i < 12; i++) {
+  let newDiv = document.createElement('div');
+  newDiv.classList.add('series-frame');
+  let newTitle = document.createElement('h2');
+  newTitle.innerText = 'The Story of Tau';
+  let newParagraph = document.createElement('p');
+  newParagraph.innerText = `${episodes[i].title}
+${episodes[i].duration} minutes
+${episodes[i].watchedText}
+`;
+  newDiv.append(newTitle);
+  newDiv.append(newParagraph);
+  if (episodes[i].continueWatching) {
+    let newButton = document.createElement('button');
+    newButton.innerText = 'Continue watching';
+    newParagraph.append(newButton);
+  }
+  body.append(newDiv);
+}
+*/
+
