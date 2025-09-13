783b9ac527a6543372cc4d9c4514255fb8721625
Peter Adjao <pa376499@gmail.com>
2025-09-13 06:03:29 +0000

    feat: Implementent a side bar to display an option when selected on a form

--- js/form.js
+++ js/form.js
@@ -1,8 +1,13 @@
 // Form and Sidebar
 
-const hobbiesResult = document.querySelector("#hobbies-result");
+const hobbiesResult = document.querySelector("#hobbies-result")
 const transportResult = document.querySelector("#transport-result");
 const musicResult = document.querySelector("#music-result"); 
+
+const radioButtons = document.querySelectorAll('input[name="transport-method"]');
+const musicDropdown = document.getElementById("music-preference");
+
+musicResult.textContent = musicDropdown.value;
 
 //capturing the event object and adding event listener to the checkbox
 document.querySelector("#sports-checkbox").addEventListener("change", ($event) => {
@@ -28,6 +33,25 @@ document.querySelector("#music-checkbox").addEventListener("change", ($event) =>
         hobbiesResult.children[2].classList.add("gray-text");
     }
 });
+
+/*
+*Radio buttons event listeners
+*/
+
+for (let i = 0; i < radioButtons.length; i++) {
+    radioButtons[i].addEventListener("change", ($event) => {
+        transportResult.textContent = $event.target.value;
+    });
+}
+
+/*
+*Dropsdown menu event listeners
+*/
+
+musicDropdown.addEventListener("change", ($event) => {
+    musicResult.textContent = $event.target.value;
+});
+
 
 
 
