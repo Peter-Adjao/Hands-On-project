dc864262eac326a57788ad27d7de6f5c217b95d5
Peter Adjao <pa376499@gmail.com>
2025-09-15 05:22:56 +0000

    feat: Implementent html constraints and blur event to validate user input on a form

--- js/form.js
+++ js/form.js
@@ -51,6 +51,71 @@ for (let i = 0; i < radioButtons.length; i++) {
 musicDropdown.addEventListener("change", ($event) => {
     musicResult.textContent = $event.target.value;
 });
+//******************************************************************
+
+//Reference holders for password and button
+
+const passwordInput = document.querySelector("#password-input");
+const submitButton = document.querySelector("#submit-button");
+
+const confirmPassword = document.querySelector("#confirm-password");
+const errorMessage = document.querySelector("#error-message");
+
+// passwordInput.addEventListener("input", (e) => {
+//     if (e.target.value.length >= 6 && e.target.value.length <= 12) {
+//         //submitButton.removeAttribute("disabled");
+//     } else {
+//         submitButton.setAttribute("disabled", "true");
+//     }
+// });
+
+confirmPassword.addEventListener("blur", () => {//remove error message and apply green classList
+    if (passwordInput.value === confirmPassword.value && passwordInput.value.length >= 6 && confirmPassword.value.length <= 12 ) {
+        passwordInput.style.border = "thin solid green";
+        confirmPassword.style.border = "thin solid green";
+        errorMessage.style.display = "none";
+        submitButton.removeAttribute("disabled");
+    } else {//show error message and apply red classList
+        passwordInput.style.border = "thin solid red";
+        confirmPassword.style.border = "thin solid red";
+        errorMessage.style.display = "inline";
+        submitButton.setAttribute("disabled", "true");
+    }
+});
+
+//*****************************************************************************************
+//Access DoM elements
+
+const reportSection = document.querySelector("wether-report");
+const cityForm = document.querySelector("city-form");
+const cityInput = document.querySelector("city");
+
+// Prepare openweathermap.org request
+let apiRequest = new XMLHttpRequest();
+
+/* 
+ * Capture and handle form submit event
+ * Prevent default behaviour, prepare and send API request
+*/
+
+cityForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const chosenCity = cityInput.value;
+    apiRequest.open('GET','https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e'); apiRequest.send();
+
+});
+
+apiRequest.onreadystatechange = () => {
+    if (apiRequest.ready === 4) {
+        if (apiRequest.status === 404) {
+            return reportSection.textContent = "City not found!";
+        }
+        const response  = JSON.parse(apiRequest.response);
+        reportSection.textContent = `The weather in` + response.name + `is` + response.weather[0].main + `.`;
+    }
+}
+
+
 
 
 
