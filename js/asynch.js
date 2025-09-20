eefc78a5af4eb56a661e392a8045916e24b9c137
Peter Adjao <pa376499@gmail.com>
2025-09-20 15:51:36 +0000

    feat: Implement a promise with an asynchronous function

--- js/asynch.js
+++ js/asynch.js
@@ -1,16 +1,39 @@
-// Accessin the DOM
+// // Accessin the DOM using assyn call
+// const result = document.querySelector("#result");
+ 
+// let approval = "Not Approved!";
+
+// function getApproval(callback) {
+//     setTimeout(() => {
+//         approval = "Approved!";
+//         callback();                                              
+//     }, 500);
+// }
+
+// getApproval(() => {
+//     result.textContent = approval;
+// });
+// result.textContent = approval;
+//************************************************************
+
+/*
+*Accessing and manipulating the Dom using async and promise
+*/
 const result = document.querySelector("#result");
  
 let approval = "Not Approved!";
 
-function getApproval(callback) {
-    setTimeout(() => {
-        approval = "Approved!";
-        callback();                                              
+function getApproval() {
+   return new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        resolve("Approved!");
     }, 500);
+   });
 }
 
-getApproval(() => {
-    result.textContent = approval;
-});
+getApproval().then(
+    (resolvedApproval) => {
+        result.textContent = resolvedApproval;
+    }
+);
 result.textContent = approval;
